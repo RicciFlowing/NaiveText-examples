1424aace7eac554dc3fa50706d0bcee25dc4e3de
RicciFlowing <benjamin@mathe-sellin.de>
2015-11-16 15:01:59 +0100

    Added scopes to posts

--- easy/spam_filter_2/app/models/post.rb
+++ easy/spam_filter_2/app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+  scope :spam, ->{ where(content_type: 'spam') }
+  scope :content, ->{ where.not(content_type: 'spam') }
+  scope :verified, ->{ where(content_type: 'verified_content') }
 end
