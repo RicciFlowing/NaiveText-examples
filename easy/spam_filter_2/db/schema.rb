f891f6b15c09d2bc717de6a99983f385ad41ec31
RicciFlowing <benjamin@mathe-sellin.de>
2015-11-16 14:57:59 +0100

    Added content-type to posts

--- easy/spam_filter_2/db/schema.rb
+++ easy/spam_filter_2/db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151116135326) do
+ActiveRecord::Schema.define(version: 20151116135550) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "text"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.string   "content_type"
   end
 
 end
