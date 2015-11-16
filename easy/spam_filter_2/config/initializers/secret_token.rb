3a3b4121862c8f956d00ebbe8c49bfeab50d3211
RicciFlowing <benjamin@mathe-sellin.de>
2015-11-16 14:55:13 +0100

    initialized the new spam fitler example

---
+++ easy/spam_filter_2/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SpamFilter2::Application.config.secret_key_base = 'a068c756ea6ae5c21943bcb26beb4d7e369df8a5118255fa6338ec4f78fb22654e9ff084c17ee3a5db31fdca1592665f2900822e6eedc178415fda08a422705b'
